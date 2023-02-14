057d1c6ac8422b528a41abbc133cd09624154225
Shawn Renner <shawn.renner@gmail.com>
2023-02-14 12:53:03 -0600

    add font awesome

--- web/src/main.js
+++ web/src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
 import "./assets/main.css";
 
-const app = createApp(App);
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faRecycle } from '@fortawesome/free-solid-svg-icons';
+library.add(faRecycle);
+
+const app = createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon);
 
 app.use(router);
 
