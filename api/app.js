1bd13f9c88f2bd5e2b6332ae87d710f85cd58c3f
Shawn Renner <shawn.renner@gmail.com>
2023-02-14 22:11:19 -0600

    use local date for heartbeat

--- api/app.js
+++ api/app.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/api/heartbeat', (req, res) => {
-  res.send(new Date().toISOString());
+  let date = new Date();
+  let localDate = new Date(date.getTime() - date.getTimezoneOffset()*60000);
+  res.send(localDate.toISOString());
 });
 
 app.listen(port, () => {
