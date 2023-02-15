b2f822ef98552869aca7716ef1c0b3e99c92649d
Shawn Renner <shawn.renner@gmail.com>
2023-02-14 21:52:31 -0600

    basic heartbeat endpoint

--- api/app.js
+++ api/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/api/heartbeat', (req, res) => {
+  res.send(new Date().toISOString());
 });
 
 app.listen(port, () => {
