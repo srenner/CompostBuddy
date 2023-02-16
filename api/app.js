443fe0eb948edb602aa34ba1b956395977aac4fb
Shawn Renner <shawn.renner@gmail.com>
2023-02-16 07:57:16 -0600

    org

--- api/app.js
+++ api/app.js
@@ -3,45 +3,29 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
-
-
-
 const { MongoClient } = require("mongodb");
-const uri = "mongodb://localhost:27017/";
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    await client.connect();
-    await client.db("compost").command({ ping: 1 });
-    console.log("Connected successfully to server");
 
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+app.listen(port, () => {
+    console.log(`CompostBuddy API listening on port ${port}`);
+  });
 
-
-
-
-
-
-
-
-
+// GET ////////////////////////////////////////////////////////////////////////
 
 app.get('/api/datetime', (req, res) => {
     let date = new Date();
-    let localDate = new Date(date.getTime() - date.getTimezoneOffset()*60000);
+    let localDate = 
+        new Date(date.getTime() - date.getTimezoneOffset()*60000);
     res.send(localDate.toISOString());
 });
 
 app.get('/api/version', (req, res) => {
     res.send('1.0.0');
 });
+
+
+
+
+// POST ///////////////////////////////////////////////////////////////////////
 
 app.post('/api/compost', function(req, res) {
 
@@ -67,8 +51,4 @@ app.post('/api/compost', function(req, res) {
         'lastTurn': lastTurn,
         'batt': batt+1
     });
-});
-
-app.listen(port, () => {
-  console.log(`CompostBuddy API listening on port ${port}`);
 });
