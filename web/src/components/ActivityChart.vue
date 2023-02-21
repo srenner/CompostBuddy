6654a322559190343e86bb3677ee9d8c5b4687c7
Shawn Renner <shawn.renner@gmail.com>
2023-02-21 12:23:28 -0600

    put data on chart

--- web/src/components/ActivityChart.vue
+++ web/src/components/ActivityChart.vue
@@ -3,13 +3,35 @@ import { reactive } from 'vue'
 import axios from 'axios';
 import Chart from 'chart.js/auto';
 
-const state = reactive({ apiVersion: 'unknown' });
+const state = reactive({ apiVersion: 'unknown', events: [] });
 
 const minutesInDay = 1440; // should be pretty close to records per day
 
-axios.get('http://localhost:3000/api/version')
+axios.get('http://localhost:3000/api/events?start=2023-02-16&end=2023-02-18')
   .then(function (response) {
-    state.apiVersion = response.data;
+    state.events = response.data;
+
+
+    new Chart(
+    document.getElementById('activity1'),
+    {
+      type: 'line',
+      data: {
+        labels: state.events.map(row => row.timestamp),
+        datasets: [
+        {
+          label: 'temp1',
+          data: state.events.map(row => row.bin1.temp)
+        },
+        {
+          label: 'temp2',
+          data: state.events.map(row => row.bin2.temp)
+        }
+        ]
+      }
+    });
+
+
   })
   .catch(function (error) {
     console.log(error);
@@ -17,11 +39,23 @@ axios.get('http://localhost:3000/api/version')
   .finally(function () {
     // 
   });
+
+
+
+
+
+
+
+
+
+
+
 </script>
 
 <template>
     <div>chart goes here</div>
-    <div style="width: 800px;"><canvas id="activity"></canvas></div>
+    <div style="width: 800px;"><canvas id="activity1"></canvas></div>
+    <div class="d-none">{{ state.events }}</div>
 </template>
 
 <style scoped>
