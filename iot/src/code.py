4cc7fb8f0341ba57812b34c23e9306e8dfcf3eed
Shawn Renner <shawn.renner@gmail.com>
2023-02-24 12:08:47 -0600

    settings, timings, progress

--- iot/src/code.py
+++ iot/src/code.py
@@ -11,9 +11,10 @@ import adafruit_icm20x
 import array
 import colors
 import secrets
+import settings
+import json
 
 # SETUP ##########################################################################
-
 
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
@@ -32,11 +33,19 @@ bin2_temp = 10.0
 turn_buffer = array.array('f', [0,0,0,0,0,0,0,0,0,0])
 tb_idx = 0
 
+current_time = time.time()
+next_json = current_time + settings.json_interval
+next_post = current_time + settings.post_interval
+
 pixels.fill(colors.led_off)
 
 # LOGIC ##########################################################################
 
+#print(time.time())
+
 while True:
+
+
     current_gyro = Functions.get_gyro_motion(icm.gyro)
 
     if tb_idx > 9:
@@ -52,11 +61,19 @@ while True:
 
     was_turning = is_turning
 
-    if secrets.DEBUG:
+    if settings.DEBUG:
         if is_turning:
             print("turning now")
         else:
             print(f"last turn", last_turn)
+
+    current_time = time.time()
+    if current_time >= next_json:
+        print("create json object now")
+        next_json = current_time + settings.json_interval
+    if current_time >= next_post:
+        print("post data now")
+        next_post = current_time + settings.post_interval
 
     time.sleep(1)
 
