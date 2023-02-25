30c7ba91b8cfa14e0f1c90583f639da2fbdc7ee7
Shawn Renner <shawn.renner@gmail.com>
2023-02-24 21:35:46 -0600

    get temperature values

--- iot/src/code.py
+++ iot/src/code.py
@@ -13,6 +13,7 @@ import colors
 import secrets
 import settings
 import json
+import adafruit_thermistor
 
 # SETUP ##########################################################################
 
@@ -23,12 +24,16 @@ pixels.fill(colors.led_red)
 i2c = board.STEMMA_I2C()
 icm = adafruit_icm20x.ICM20948(i2c)
 
+therm1 = adafruit_thermistor.Thermistor(board.A0, 10000, 10000, 25, 3950)
+therm2 = adafruit_thermistor.Thermistor(board.A1, 10000, 10000, 25, 3950)
+bin1_temp = 0.0
+bin1_buf = [0.0,0.0,0.0]
+bin2_temp = 0.0
+bin2_buf = [0.0,0.0,0.0]
+
 is_turning = False
 was_turning = False
 last_turn = 'unknown'
-
-bin1_temp = 10.0
-bin2_temp = 10.0
 
 turn_buffer = array.array('f', [0,0,0,0,0,0,0,0,0,0])
 tb_idx = 0
@@ -70,6 +75,24 @@ while True:
     current_time = time.time()
     if current_time >= next_json:
         print("create json object now")
+
+        bin1_buf[0] = therm1.temperature
+        bin2_buf[0] = therm2.temperature
+        time.sleep(.1)
+        bin1_buf[1] = therm1.temperature
+        bin2_buf[1] = therm2.temperature
+        time.sleep(.1)
+        bin1_buf[2] = therm1.temperature
+        bin2_buf[2] = therm2.temperature
+
+        bin1_temp = sum(bin1_buf) / len(bin1_buf)
+        bin2_temp = sum(bin2_buf) / len(bin2_buf)
+
+        print(f"bin1: {bin1_buf}")
+        print(f"bin1: {bin1_temp}")
+        print(f"bin2: {bin2_buf}")
+        print(f"bin2: {bin2_temp}")
+
         next_json = current_time + settings.json_interval
     if current_time >= next_post:
         print("post data now")
