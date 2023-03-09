8644d0503d3101051009f5d7688ff9a47ac430bb
Shawn Renner <shawn.renner@gmail.com>
2023-03-09 12:52:10 -0600

    remove debug msg, show more data

--- web/src/components/ActivityChart.vue
+++ web/src/components/ActivityChart.vue
@@ -9,7 +9,7 @@ const minutesInDay = 1440; // should be pretty close to records per day
 
 axios.get('http://localhost:3000/api/events?start=2023-03-08&end=2023-03-18')
   .then(function (response) {
-    state.events = response.data.slice(-30);
+    state.events = response.data.slice(-60);
 
     new Chart(
     document.getElementById('activity1'),
@@ -55,7 +55,6 @@ axios.get('http://localhost:3000/api/events?start=2023-03-08&end=2023-03-18')
 </script>
 
 <template>
-    <div>chart goes here</div>
     <div style="width: 800px;"><canvas id="activity1"></canvas></div>
     <div class="d-none">{{ state.events }}</div>
 </template>
