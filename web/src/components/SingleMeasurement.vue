2699a2a7c28e0062695655ca965502ae10b08f3b
Shawn Renner <shawn.renner@gmail.com>
2023-03-12 16:10:38 -0500

    show single measurement on screen

--- web/src/components/SingleMeasurement.vue
+++ web/src/components/SingleMeasurement.vue
@@ -1,26 +1,59 @@
 <script setup>
-//import { reactive } from 'vue'
+import { reactive } from 'vue';
+import axios from 'axios';
 
-defineProps({
-  measurement: {},
-});
+// defineProps({
+//   measurement: {},
+// });
 
+const state = reactive({ measurement: {} });
+
+axios.get('http://localhost:3000/api/event/latest')
+  .then(function (response) {
+    state.measurement = response.data;
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+    // 
+  });
 
 </script>
 
 <template>
 
-  {{ measurement }}
+  <div class="d-none">
+    {{ state.measurement }}
+  </div>
+  
+
+  <h3 class="d-none">Power Status</h3>
+  <dl>
+    <dt>Battery Voltage</dt>  
+    <dd>{{ state.measurement.vbat }}</dd>
+
+    <dt>Power Active</dt>
+    <dd> {{ state.measurement.power }} </dd>
+
+    <dt>Charging</dt>
+    <dd>{{ state.measurement.charging }}</dd>
+  </dl>
+
+  <dl>
+    <dt>Temperature 1</dt>
+    <dd>{{ state.measurement.temp1 }}</dd>
+
+    <dt>Temperature 2</dt>
+    <dd>{{ state.measurement.temp2 }}</dd>
+
+    <dt>Last Turn</dt>
+    <dd>{{ state.measurement.last_turn || 'unknown' }}</dd>
+  </dl>
 
 
   <div class="small">
-    <div class="alert alert-info" role="alert" v-for="info in measurement.info">
-      {{ info }}
-    </div>
-    <div class="alert alert-warning" role="alert" v-for="warning in measurement.warnings">
-      {{ warning }}
-    </div>
-    <div class="alert alert-danger" role="alert" v-for="error in measurement.errors">
+    <div class="alert alert-danger" role="alert" v-for="error in state.measurement.errors">
       {{ error }}
     </div>
   </div>
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -2,21 +2,15 @@
 import PageHeader from "../components/PageHeader.vue";
 import ActivityChart from "../components/ActivityChart.vue";
 import MessageCenter from "../components/MessageCenter.vue";
+import SingleMeasurement from "../components/SingleMeasurement.vue";
 
 </script>
 
 <template>
   <main class="container">
-    
-    
     <PageHeader title="Activity" icon="fa-solid fa-chart-line" />
-
     <ActivityChart />
-    
+    <SingleMeasurement />
     <!-- <MessageCenter /> -->
-
-    
-
-
   </main>
 </template>
