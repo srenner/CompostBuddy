b1e79c4f3bc42b0d8d070771a1327401b34e91bb
Shawn Renner <shawn.renner@gmail.com>
2023-03-12 16:17:05 -0500

    tweak ui, use columns

--- web/src/components/SingleMeasurement.vue
+++ web/src/components/SingleMeasurement.vue
@@ -21,44 +21,56 @@ axios.get('http://localhost:3000/api/event/latest')
 
 </script>
 
+
 <template>
 
   <div class="d-none">
     {{ state.measurement }}
   </div>
   
+  <div class="row">
+    <div class="col-md-auto">
+      <dl>
+        <dt>Battery Voltage</dt>  
+        <dd>{{ state.measurement.vbat }}</dd>
 
-  <h3 class="d-none">Power Status</h3>
-  <dl>
-    <dt>Battery Voltage</dt>  
-    <dd>{{ state.measurement.vbat }}</dd>
+        <dt>Power Active</dt>
+        <dd> {{ state.measurement.power }} </dd>
 
-    <dt>Power Active</dt>
-    <dd> {{ state.measurement.power }} </dd>
+        <dt>Charging</dt>
+        <dd>{{ state.measurement.charging }}</dd>
+      </dl>
+    </div>
+    <div class="col-md-auto">
+      <dl>
+        <dt>Temperature 1</dt>
+        <dd>{{ state.measurement.temp1 }}</dd>
 
-    <dt>Charging</dt>
-    <dd>{{ state.measurement.charging }}</dd>
-  </dl>
+        <dt>Temperature 2</dt>
+        <dd>{{ state.measurement.temp2 }}</dd>
 
-  <dl>
-    <dt>Temperature 1</dt>
-    <dd>{{ state.measurement.temp1 }}</dd>
-
-    <dt>Temperature 2</dt>
-    <dd>{{ state.measurement.temp2 }}</dd>
-
-    <dt>Last Turn</dt>
-    <dd>{{ state.measurement.last_turn || 'unknown' }}</dd>
-  </dl>
-
-
-  <div class="small">
-    <div class="alert alert-danger" role="alert" v-for="error in state.measurement.errors">
-      {{ error }}
+        <dt>Last Turn</dt>
+        <dd>{{ state.measurement.last_turn || 'unknown' }}</dd>
+      </dl>
+    </div>
+    <div class="col">
+      <div class="alert alert-danger" role="alert" v-for="error in state.measurement.errors">
+        {{ error }}
+      </div>
+      <!--uncomment to view example of error message format-->
+      <div class="d-none">
+        <div class="alert alert-danger" role="alert" >
+          Repeated socket errors
+        </div>
+        <div class="alert alert-danger" role="alert" >
+          Network Error 6
+        </div>
+      </div>
     </div>
   </div>
-    
+  
 </template>
+
 
 <style scoped>
 
