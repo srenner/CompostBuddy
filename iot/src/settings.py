cb08dbc75484405669ae4fe1d07a06a70adf6f74
Shawn Renner <shawn.renner@gmail.com>
2023-03-16 09:31:15 -0500

    sleep longer

--- iot/src/settings.py
+++ iot/src/settings.py
@@ -1,5 +1,5 @@
 DEBUG = False                       # extra print statements
-loop_interval = 1.5                 # sleep time between loops
+loop_interval = 2                   # sleep time between loops
 json_interval = 60                  # interval for building a json object
 post_interval = 1200                # interval for POSTing the array of json objects
 low_power_multiplier = 1            # multiply all the above values when battery is low (value set in code)
